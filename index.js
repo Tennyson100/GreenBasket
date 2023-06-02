1421cbee2d116719b566a0b1726e3f6d31f50c3c
Gaurav Prateek <131291701+Tennyson100@users.noreply.github.com>
2023-06-02 07:28:57 +0530

    Update index.js
    mongo connection link update

--- index.js
+++ index.js
@@ -101,20 +101,11 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-/* 
-
-Site dubara se host krte waqt yaha pr changes krna ha.
-.env file bana kr waha pr database ka password username store hoga
-and frr gitignore.
-
-
-
-*/
 
 
 mongoose
   .connect(
-    process.env.DB_URL || 3000,
+    process.env.DB_URL || 'mongodb://localhost:27017/mis',
     { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true },
   )
   .then(() => app.listen(process.env.PORT || 5000, console.log("Your server is up man.....")))
